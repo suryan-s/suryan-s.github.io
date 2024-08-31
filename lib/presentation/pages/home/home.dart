eaaba15183308a2d3481e1eba13f578c38cf72ea
suryan-s <suryannasa@gmail.com>
2024-08-31 19:55:02 +0530

    feat: Flutter setup finished with clean architecture format

---
+++ lib/presentation/pages/home/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Text(
+              'Main page',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
